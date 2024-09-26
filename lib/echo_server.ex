e175db78c71084af0323a0c9e69fd73520b922de
Радмир Шапинский <r.shapinskiy@tdm-tech.ru>
2024-09-26 13:47:26 +0400

    Эхо сервер ping -> pong

--- lib/echo_server.ex
+++ lib/echo_server.ex
@@ -1,18 +1,25 @@
 defmodule EchoServer do
-  @moduledoc """
-  Documentation for `EchoServer`.
-  """
+  use GenServer
 
-  @doc """
-  Hello world.
+  # Client
 
-  ## Examples
+  def start_link() do
+    GenServer.start_link(__MODULE__, [])
+  end
 
-      iex> EchoServer.hello()
-      :world
+  def send_message(pid, :ping) do
+    GenServer.call(pid,  {:send_message, :ping})
+  end
 
-  """
-  def hello do
-    :world
+  # Server (callbacks)
+
+  @impl true
+  def init(init_args) do
+    {:ok, init_args}
+  end
+
+  @impl true
+  def handle_call({:send_message, :ping}, _from, state) do
+    {:reply, {:pong, Node.self()}, state}
   end
 end
